e0a79cf6946bf061ff5c55d58963afce55f81f9f
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-15 21:48:18 +0300

    Student API Routes Overhauled.

--- src/services/student/router.ts
+++ src/services/student/router.ts
@@ -1,47 +1,58 @@
 import { Router } from "express";
 import { Student } from "./schema";
 
+export const STUDENT_ROUTE = "/student";
 export const StudentRouter = Router();
 
-StudentRouter.route("/student(/:id)?")
-    .get((request, response) =>
-        (request.params.id != null
-            ? Student.findById(request.params.id).then((student) =>
-                  student != null
-                      ? response.send(student)
-                      : response.status(404).send("Student not found."),
-              )
-            : Student.find().then((result) => response.send(result))
-        ).catch((error) => response.status(400).send(error)),
-    )
-    .post((request, response) =>
-        new Student(request.body)
-            .save()
-            .then((student) => response.send(student._id))
-            .catch((error) => response.status(400).send(error)),
-    )
-    .patch((request, response) =>
-        Student.findByIdAndUpdate(request.params.id, request.body, {
-            new: true,
-        })
-            .then((student) =>
-                student != null
-                    ? response.send("Student updated.")
-                    : response.status(404).send("Student not found."),
-            )
-            .catch((error) => response.status(400).send(error)),
-    )
-    .delete((request, response) =>
-        (request.params.id != null
-            ? Student.findByIdAndDelete(request.params.id)
-            : Student.deleteMany()
+StudentRouter.get(STUDENT_ROUTE, (_request, response) =>
+    Student.find()
+        .then((students) => response.send(students))
+        .catch((error) => response.status(400).send(error)),
+);
+
+StudentRouter.get(`${STUDENT_ROUTE}/:id`, (request, response) =>
+    Student.findById(request.params.id)
+        .then((student) =>
+            student != null
+                ? response.send(student)
+                : response.status(404).send("Student not found."),
         )
-            .then(() =>
-                response.send(
-                    request.params.id != null
-                        ? "Student deleted."
-                        : "All students deleted.",
-                ),
-            )
-            .catch((error) => response.status(400).send(error)),
-    );
+        .catch((error) => response.status(400).send(error)),
+);
+
+StudentRouter.post(STUDENT_ROUTE, (request, response) =>
+    new Student(request.body)
+        .save()
+        .then((student) => response.send(student.id))
+        .catch((error) => response.status(400).send(error)),
+);
+
+StudentRouter.patch(`${STUDENT_ROUTE}/:id`, (request, response) =>
+    Student.findByIdAndUpdate(request.params.id, request.body, { new: true })
+        .then((student) =>
+            student != null
+                ? response.send("Student has been successfully updated.")
+                : response.status(404).send("Student not found."),
+        )
+        .catch((error) => response.status(400).send(error)),
+);
+
+StudentRouter.delete(STUDENT_ROUTE, (_request, response) =>
+    Student.deleteMany()
+        .then((result) =>
+            response.send(
+                `${result.deletedCount} students has been successfully deleted.`,
+            ),
+        )
+        .catch((error) => response.status(400).send(error)),
+);
+
+StudentRouter.delete(`${STUDENT_ROUTE}/:id`, (request, response) =>
+    Student.findByIdAndDelete(request.params.id)
+        .then((student) =>
+            student != null
+                ? response.send("Student has been successfully deleted.")
+                : response.status(404).send("Student not found."),
+        )
+        .catch((error) => response.status(400).send(error)),
+);
