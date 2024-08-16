b14698b95c0f8d397d4310af57e0ab516a7b51ba
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-16 15:38:22 +0300

    Version Key Removed.

--- src/services/student/schema.ts
+++ src/services/student/schema.ts
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-export const StudentSchema = new mongoose.Schema({
-    name: String,
-    year: Number,
-    roll_number: Number,
-    subjects: Array<String>,
-});
+export const StudentSchema = new mongoose.Schema(
+    {
+        name: String,
+        year: Number,
+        roll_number: Number,
+        subjects: Array<String>,
+    },
+    { versionKey: false },
+);
 
 export const Student = mongoose.model("Student", StudentSchema);
