631a987b9d3d68b0a1d469001b1ad88ff0091686
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-17 17:50:30 +0300

    Patch Document Utility.

---
+++ src/utils/PatchDocument.ts
@@ -0,0 +1,18 @@
+import { Model } from "mongoose";
+import { RequestHandlerWithID } from "../types";
+
+export function PatchDocument<T>(model: Model<T>): RequestHandlerWithID {
+    return (request, response) =>
+        model
+            .findByIdAndUpdate(request.params.id, request.body, { new: true })
+            .then((document) =>
+                document != null
+                    ? response.send({
+                          message: `${model.modelName} has been successfully updated.`,
+                      })
+                    : response
+                          .status(404)
+                          .send({ message: `${model.modelName} not found.` }),
+            )
+            .catch((error) => response.status(500).send(error));
+}
