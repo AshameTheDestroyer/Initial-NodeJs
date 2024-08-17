046d00502b33827acbb3179b6d1ce8299b5423ee
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-17 17:49:28 +0300

    Delete Document By ID Utility.

---
+++ src/utils/DeleteDocumentByID.ts
@@ -0,0 +1,18 @@
+import { Model } from "mongoose";
+import { RequestHandlerWithID } from "../types";
+
+export function DeleteDocumentByID<T>(model: Model<T>): RequestHandlerWithID {
+    return (request, response) =>
+        model
+            .findByIdAndDelete(request.params.id)
+            .then((document) =>
+                document != null
+                    ? response.send({
+                          message: `${model.modelName} has been successfully deleted.`,
+                      })
+                    : response
+                          .status(404)
+                          .send({ message: `${model.modelName} not found.` }),
+            )
+            .catch((error) => response.status(500).send(error));
+}
