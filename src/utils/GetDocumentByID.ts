f7d08336fb28f3f86e6f23ac125c0e31cc4c8978
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-17 17:48:26 +0300

    Get Document By ID Utility.

---
+++ src/utils/GetDocumentByID.ts
@@ -0,0 +1,16 @@
+import { Model } from "mongoose";
+import { RequestHandlerWithID } from "../types";
+
+export function GetDocumentByID<T>(model: Model<T>): RequestHandlerWithID {
+    return (request, response) =>
+        model
+            .findById(request.params.id)
+            .then((document) =>
+                document != null
+                    ? response.send(document)
+                    : response
+                          .status(404)
+                          .send({ message: `${model.modelName} not found.` }),
+            )
+            .catch((error) => response.status(500).send(error));
+}
