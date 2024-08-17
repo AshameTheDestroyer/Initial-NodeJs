26b2c52dbd3948d15dca41a0d8e9e1eaad7e0a6c
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-17 22:05:04 +0300

    Multi-Role Authority Added.

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -2,18 +2,26 @@ import { RequestHandler } from "express";
 import { UserModel, UserProps } from "../services/user";
 
 export const ValidateAuthority: (
-    requiredRole: UserProps["role"],
-) => RequestHandler = (requiredRole) => (request, response, next) =>
-    UserModel.findById(
-        (request as typeof request & { userId: string })["userId"],
-    )
-        .then((user) =>
-            user == null
-                ? response.send({ message: "User isn't found." })
-                : user.role != requiredRole
-                ? response.send({
-                      message: `User is unauthorized, should have the role of "${requiredRole}".`,
-                  })
-                : next(),
+    ...requiredRoles: Array<UserProps["role"]>
+) => RequestHandler =
+    (...requiredRoles) =>
+    (request, response, next) =>
+        UserModel.findById(
+            (request as typeof request & { userId: string })["userId"],
         )
-        .catch((error) => response.status(500).send(error));
+            .then((user) =>
+                user == null
+                    ? response.send({ message: "User isn't found." })
+                    : !requiredRoles.includes(user.role)
+                    ? response.send({
+                          message:
+                              "User is unauthorized, should have the role of " +
+                              new Intl.ListFormat("en-US", {
+                                  style: "long",
+                                  type: "disjunction",
+                              }).format(requiredRoles) +
+                              ".",
+                      })
+                    : next(),
+            )
+            .catch((error) => response.status(500).send(error));
