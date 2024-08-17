34069b0bec055993100bdcf7089f095d62c1724b
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-17 17:53:16 +0300

    Search Indexes Added.

--- src/services/student/schema.ts
+++ src/services/student/schema.ts
@@ -8,9 +8,14 @@ export const StudentSchema = new mongoose.Schema(
         rollNumber: Number,
     },
     {
+        autoIndex: false,
         versionKey: false,
         timestamps: { createdAt: true, updatedAt: false },
     },
 );
 
+StudentSchema.index({ name: "text" });
+
 export const StudentModel = mongoose.model("Student", StudentSchema);
+
+StudentModel.createIndexes();
