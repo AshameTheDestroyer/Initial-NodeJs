fa2ad5748b129b44a34ab111d7089c6c49b888df
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-17 17:49:04 +0300

    Delete All Documents Utility.

---
+++ src/utils/DeleteAllDocuments.ts
@@ -0,0 +1,16 @@
+import { Model } from "mongoose";
+import { RequestHandler } from "express";
+
+export function DeleteAllDocuments<T>(model: Model<T>): RequestHandler {
+    return (_request, response) =>
+        model
+            .deleteMany()
+            .then((result) =>
+                response.send({
+                    message:
+                        result.deletedCount +
+                        ` ${model.modelName.toLowerCase()}(s) has been successfully deleted.`,
+                }),
+            )
+            .catch((error) => response.status(500).send(error));
+}
