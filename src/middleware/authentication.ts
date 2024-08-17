edc9e520fafd271ab37476f3e110cc2047797259
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-17 21:24:49 +0300

    Validate Authenticity Middleware.

---
+++ src/middleware/authentication.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from "express";
+import Jwt, { JwtPayload } from "jsonwebtoken";
+
+export const ValidateAuthenticity: RequestHandler = (
+    request,
+    response,
+    next,
+) => {
+    if (request.headers.authorization == null) {
+        response.status(400).send({ message: "User is unauthenticated." });
+        return;
+    }
+
+    try {
+        const payload = Jwt.verify(
+            request.headers.authorization.split(" ")[1],
+            "secretKey",
+        );
+
+        const _request = request as typeof request & { userId: string };
+        const _payload = payload as (JwtPayload | string) & { userId: string };
+
+        _request["userId"] = _payload["userId"];
+
+        next();
+    } catch {
+        response.status(401).send({ message: "Invalide token." });
+    }
+};
