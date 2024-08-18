9d12ac51f3a69c12b8988ef0cc435d01134c1369
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-18 19:12:16 +0300

    Email Manager.

---
+++ src/third-party/EmailManager.ts
@@ -0,0 +1,50 @@
+import nodemailer, { Transporter } from "nodemailer";
+
+export class EmailManager {
+    private static instance: EmailManager;
+
+    private email: string;
+    private transporter: Transporter;
+
+    public static get Instance() {
+        return (this.instance ??= new EmailManager());
+    }
+
+    public constructor() {
+        if (
+            process.env["NODEMAILER_EMAIL"] == null ||
+            process.env["NODEMAILER_PASSWORD"] == null
+        ) {
+            throw new Error(
+                'Environment variables "EMAIL & EMAIL_PASSWORD" aren\'t set.',
+            );
+        }
+
+        this.transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                pass: process.env["NODEMAILER_PASSWORD"],
+                user: (this.email = process.env["NODEMAILER_EMAIL"]),
+            },
+        });
+    }
+
+    public static Send(props: {
+        text: string;
+        title: string;
+        receiver: string;
+    }) {
+        this.Instance.transporter.sendMail(
+            {
+                text: props.text,
+                to: props.receiver,
+                subject: props.title,
+                from: this.Instance.email,
+            },
+            (error, info) =>
+                error
+                    ? console.error(error)
+                    : console.log(`Email sent: ${info.response}`),
+        );
+    }
+}
