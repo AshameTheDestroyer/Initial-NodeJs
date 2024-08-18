22ed9cc8633b8ed73b08d198c0ea6a5acb4161e4
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-18 15:11:28 +0300

    Password Hashing & Comparing on Sign up & Login.

--- src/services/authentication/services.ts
+++ src/services/authentication/services.ts
@@ -1,6 +1,7 @@
 import Jwt from "jsonwebtoken";
 import { RequestHandler } from "express";
 import { UserModel } from "../user/schema";
+import { CheckPassword, HashPassword } from "../../utils";
 
 export const SignUp: RequestHandler = (request, response) =>
     UserModel.findOne({ email: request.body["email"] })
@@ -9,29 +10,43 @@ export const SignUp: RequestHandler = (request, response) =>
                 ? response
                       .status(400)
                       .send({ message: "Email is already taken." })
-                : new UserModel({ ...request.body, role: "user" })
-                      .save()
-                      .then(() =>
-                          response.send({
-                              message: "User has signed up successfully.",
-                          }),
-                      ),
+                : HashPassword(request.body["password"]).then(
+                      (hashedPassword) =>
+                          new UserModel({
+                              ...request.body,
+                              role: "user",
+                              password: hashedPassword,
+                          })
+                              .save()
+                              .then(() =>
+                                  response.send({
+                                      message:
+                                          "User has signed up successfully.",
+                                  }),
+                              ),
+                  ),
         )
         .catch((error) => response.status(500).send(error));
 
 export const Login: RequestHandler = (request, response) =>
     UserModel.findOne({ email: request.body["email"] })
         .then((user) =>
-            user != null
-                ? user.password != request.body["password"]
-                    ? response
-                          .status(401)
-                          .send({ message: "Password is incorrect." })
-                    : response.send({
-                          token: Jwt.sign({ userId: user._id }, "secretKey"),
-                      })
-                : response
+            user == null
+                ? response
                       .status(401)
-                      .send({ message: "Email isn't registered." }),
+                      .send({ message: "Email isn't registered." })
+                : CheckPassword(request.body["password"], user.password).then(
+                      (isPasswordCorrect) =>
+                          isPasswordCorrect
+                              ? response.send({
+                                    token: Jwt.sign(
+                                        { userId: user._id },
+                                        "secretKey",
+                                    ),
+                                })
+                              : response.status(401).send({
+                                    message: `Password is incorrect.`,
+                                }),
+                  ),
         )
         .catch((error) => response.status(500).send(error));
