0c80f0d1dd69fa1b5de754dcde1c74d6fe4bda51
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-19 19:29:20 +0300

    Login Test Created.

---
+++ src/services/authentication/.test.ts
@@ -0,0 +1,28 @@
+import expect from "expect";
+import { UserProps } from "../user";
+import { describe, it } from "node:test";
+
+export async function TestLogin() {
+    const response = await fetch(`http://localhost:3000/authentication/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            password: "1234",
+            email: "hashem.wannous@unifi.solutions",
+        } as UserProps),
+    });
+
+    expect(response.status).toEqual(200);
+
+    const json = await response.json();
+    expect(json).toHaveProperty("token");
+
+    return json["token"] as string;
+}
+
+describe("POST /authentication/login", () =>
+    it("Log in with an account.", () => {
+        TestLogin();
+    }));
