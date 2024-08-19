164fa888da2492b50c1966af58be1f92d19d9032
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-19 12:15:59 +0300

    Update Routes to Adopt New Authentication Services.

--- src/services/authentication/router.ts
+++ src/services/authentication/router.ts
@@ -1,8 +1,28 @@
 import { Router } from "express";
-import { Login, SignUp } from "./services";
+// import { EmailManager } from "../../third-party/EmailManager";
+import { Login, SignUp, ForgotPassword, ResetPassword } from "./services";
 
 export const AUTHENTICATION_ROUTE = "/authentication";
 export const AuthenticationRouter = Router();
 
+export const RESET_TOKEN_EXPIRATION_TIME = 60 * 60 * 1000;
+
 AuthenticationRouter.post(`${AUTHENTICATION_ROUTE}/signup`, SignUp);
 AuthenticationRouter.post(`${AUTHENTICATION_ROUTE}/login`, Login);
+
+AuthenticationRouter.post(
+    `${AUTHENTICATION_ROUTE}/forgot-password`,
+    ForgotPassword(RESET_TOKEN_EXPIRATION_TIME, (props) => {
+        // EmailManager.Send({
+        //     receiver: props.email,
+        //     title: "Password Reset Token",
+        //     text: `This is your password reset token: "${props.resetToken}".\nIf you didn't ask for this, please ignore this message.`,
+        // });
+
+        console.log(props.resetToken);
+    }),
+);
+AuthenticationRouter.post(
+    `${AUTHENTICATION_ROUTE}/reset-password`,
+    ResetPassword,
+);
