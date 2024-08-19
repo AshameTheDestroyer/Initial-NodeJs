6daa59ae9343387d983e2595d3bec5164b5441ef
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-19 19:29:43 +0300

    Get All Students Test Created.

---
+++ src/services/student/.test.ts
@@ -0,0 +1,21 @@
+import expect from "expect";
+import { describe, it } from "node:test";
+import { TestLogin } from "../authentication";
+
+export async function TestGetAllStudents() {
+    const token = await TestLogin();
+    const response = await fetch(`http://localhost:3000/student`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+        },
+    });
+
+    expect(response.status).toEqual(200);
+
+    const json = await response.json();
+    expect(json).toHaveProperty("data");
+}
+
+describe("GET /student", () => it("Get all students.", TestGetAllStudents));
