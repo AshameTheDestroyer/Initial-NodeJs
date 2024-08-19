7cbbdf7abcec3171ddae5cb4a1b715d8eae897ca
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-19 12:12:25 +0300

    Reset Token Properties.

--- src/services/user/schema.ts
+++ src/services/user/schema.ts
@@ -3,9 +3,12 @@ import mongoose from "mongoose";
 export const UserSchema = new mongoose.Schema(
     {
         name: { type: String, required: true },
+        password: { type: String, required: true },
         email: { type: String, required: true, unique: true },
-        password: { type: String, required: true, select: false },
         role: { type: String, enum: ["user", "admin"], default: "user" },
+
+        _resetToken: String,
+        _resetTokenExpirationDate: Number,
     },
     {
         autoIndex: false,
