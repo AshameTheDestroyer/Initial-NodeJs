209862d4dcb6978d6b25217c3e5f623d4deda678
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-21 21:38:09 +0300

    Patch My User Test.

--- src/services/user/.test.ts
+++ src/services/user/.test.ts
@@ -1,9 +1,20 @@
 import expect from "expect";
+import { UserProps } from "./types";
 import { describe, it } from "node:test";
 import { TestAgent } from "../authentication";
 
 async function TestGetMyUser() {
     const response = await TestAgent.Fetch("/user/mine", "GET");
+    expect(response.status).toEqual(200);
+
+    const json = await response.json();
+    expect(json).toHaveProperty("_id");
+}
+
+async function TestPatchMyUser() {
+    const response = await TestAgent.Fetch("/user/mine", "PATCH", {
+        name: "Updated Test Agent",
+    } as UserProps);
     expect(response.status).toEqual(200);
 
     const json = await response.json();
@@ -21,6 +32,11 @@ async function TestGetAllUsers() {
 TestAgent.OnAuthenticate(() =>
     describe("GET /user/mine", () =>
         it("My user data was fetched successfully.", TestGetMyUser)),
+);
+
+TestAgent.OnAuthenticate(() =>
+    describe("PATCH /user/mine", () =>
+        it("My user data was patched successfully.", TestPatchMyUser)),
 );
 
 TestAgent.OnAuthenticate(() =>
