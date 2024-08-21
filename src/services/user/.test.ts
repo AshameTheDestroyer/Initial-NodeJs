9bb6fe542cf2b1892f54c82109201fe805475288
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-21 16:46:29 +0300

    Get All Users Test.

--- src/services/user/.test.ts
+++ src/services/user/.test.ts
@@ -10,7 +10,20 @@ async function TestGetMyUser() {
     expect(json).toHaveProperty("_id");
 }
 
+async function TestGetAllUsers() {
+    const response = await TestAgent.Fetch("/user", "GET");
+    expect(response.status).toEqual(200);
+
+    const json = await response.json();
+    expect(json).toHaveProperty("data");
+}
+
 TestAgent.OnAuthenticate(() =>
     describe("GET /user/mine", () =>
         it("My user data was fetched successfully.", TestGetMyUser)),
+);
+
+TestAgent.OnAuthenticate(() =>
+    describe("GET /user", () =>
+        it("All users were fetched successfully.", TestGetAllUsers)),
 );
