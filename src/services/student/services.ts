7907f97ea5a1c69353b7f74bfb70ef3d76e2feb7
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-24 19:03:26 +0300

    Student's Subjects Fetched Only By ID.

--- src/services/student/services.ts
+++ src/services/student/services.ts
@@ -1,15 +1,34 @@
 import { StudentModel } from "./schema";
+import { SubjectModel } from "../subject";
+import { RequestHandlerWithID } from "../../types";
 import {
     PostDocument,
     GetDocuments,
     PatchDocument,
-    GetDocumentByID,
     DeleteAllDocuments,
     DeleteDocumentByID,
 } from "../../utils";
 
-export const GetStudents = GetDocuments(StudentModel);
-export const GetStudentByID = GetDocumentByID(StudentModel);
+export const GetStudents = GetDocuments(StudentModel, "-subjects");
+export const GetStudentByID: RequestHandlerWithID = (request, response) =>
+    StudentModel.findById(request.params.id)
+        .then((student) =>
+            student == null
+                ? response.status(404).send({ message: "Student isn't found." })
+                : SubjectModel.find({
+                      $or: student.subjects.map((subjectID) => ({
+                          _id: subjectID,
+                      })),
+                  }).then((subjects) =>
+                      response.send({
+                          ...student["_doc" as keyof typeof student],
+                          subjects,
+                      }),
+                  ),
+        )
+        .catch(
+            (error) => (console.error(error), response.status(500).send(error)),
+        );
 
 export const PostStudent = PostDocument(StudentModel);
 
