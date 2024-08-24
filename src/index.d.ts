7fc1d45dcbeeb7f57025c7011212c873a2e22210
hashem-wannous <hashem.wannous@unifi.solutions>
2024-08-24 15:10:14 +0300

    Types Moved.

--- src/index.d.ts
+++
@@ -1,3 +0,0 @@
-type StrictOmit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-
-type RequestMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
 import { RequestHandler } from "express";
 
 export type RequestHandlerWithID = RequestHandler<{ id: string }>;
+export type RequestMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+export type StrictOmit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
